4edd3efadcd21da3c4b7c9b6266069e95ab6d86b
uwusucculents <78513368+uwusucculents@users.noreply.github.com>
2021-04-24 20:10:50 -0700

    Add files via upload
    i tried lol

--- index.js
+++ index.js
@@ -2,12 +2,58 @@ var aaaaa = new Audio();
 aaaaa.src = "aaaa.mp3";
 var guh_huh = new Audio();
 guh_huh.src = "Cranberry Animated - Sprite Cranberry TV Commercial, Featuring LeBron James.mp3"
-
+var bbbbb = new Audio();
+bbbbb.src = "";
+var ccccc = new Audio();
+ccccc.src = "";
+var ddddd = new Audio();
+ddddd.src = "";
+var eeeee = new Audio();
+eeeee.src = "";
+var fffff = new Audio();
+fffff.src = "";
+var ggggg = new Audio();
+ggggg.src = "";
 
 var muteToggle = document.getElementById("soundToggle").checked;
 
-function soundPlay(){
+function soundPlayA(){
     if (muteToggle == false){
         aaaaa.play();
+    }
+}
+function soundPlayGuhHuh(){
+    if (muteToggle == false) {
+        guh_huh.play();
+    }
+}
+function soundPlayB(){
+    if (muteToggle == false){
+        bbbbb.play();
+    }
+}
+function soundPlayC(){
+    if (muteToggle == false){
+        ccccc.play();
+    }
+}
+function soundPlayD(){
+    if (muteToggle == false){
+        ddddd.play();
+    }
+}
+function soundPlayE(){
+    if (muteToggle == false){
+        eeeee.play();
+    }
+}
+function soundPlayF(){
+    if (muteToggle == false){
+        fffff.play();
+    }
+}
+function soundPlayG(){
+    if (muteToggle == false){
+        ggggg.play();
     }
 }
